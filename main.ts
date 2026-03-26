a964fe543cc5581b14ef154b59c2de189fbfb155
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-03-26 12:21:38 -0400

    CODE

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Mar 2026
+ * This program displays the distance between things and show led too
 */
 
-basic.showString('Hello, World!')
+//variables
+let currentdistance : number
+let neopixelStrip: neopixel.Strip = null
+
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+//cleanup
+basic.clearScreen()
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+//Press A
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    currentdistance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+    basic.showString(currentdistance.toString())
+
+//light up led
+if (currentdistance < 10) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+
+} else {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+}})
